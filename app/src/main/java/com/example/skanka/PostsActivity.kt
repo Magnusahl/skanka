a1b40de62481b1a42c046884b25b5b8f80886cd3
Magnus <51696488+Magnusahl@users.noreply.github.com>
2020-05-08 14:42:11 +0200

    working app

--- app/src/main/java/com/example/skanka/PostsActivity.kt
+++ app/src/main/java/com/example/skanka/PostsActivity.kt
@@ -6,29 +6,49 @@ import android.os.Bundle
 import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.RelativeLayout
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.skanka.model.Post
 import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.Query
+import kotlinx.android.synthetic.main.activity_posts.*
 
 class PostsActivity : AppCompatActivity() {
 
     private lateinit var firestoreDb: FirebaseFirestore
+    private lateinit var posts: MutableList<Post>
+    private lateinit var adapter: PostsAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_posts)
 
+        posts = mutableListOf()
+
+        adapter = PostsAdapter(this, posts)
+
+        rvPosts.adapter = adapter
+        rvPosts.layoutManager = LinearLayoutManager(this)
+
         firestoreDb = FirebaseFirestore.getInstance()
-        val postsReference = firestoreDb.collection("posts")
+        val postsReference = firestoreDb
+            .collection("posts")
+            .limit(20)
+            .orderBy("creation_time_ms", Query.Direction.DESCENDING)
         postsReference.addSnapshotListener { snapshot, exception ->
             if (exception != null || snapshot == null) {
                 return@addSnapshotListener
             }
             val postList = snapshot.toObjects(Post::class.java)
+            posts.clear()
+            posts.addAll(postList)
+            adapter.notifyDataSetChanged()
             for (post in postList) {
                 println("!!Datacreated ${post}")
             }
         }
-
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
--- app/src/main/java/com/example/skanka/PostsAdapter.kt
+++ app/src/main/java/com/example/skanka/PostsAdapter.kt
@@ -1,22 +1,25 @@
 package com.example.skanka
 
 import android.content.Context
+import android.text.format.DateUtils
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.example.skanka.model.Post
 import kotlinx.android.synthetic.main.new_post.view.*
 
 class PostsAdapter (val context: Context, val posts: List<Post>) :
     RecyclerView.Adapter<PostsAdapter.ViewHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        val view = LayoutInflater.from(context).inflate(R.layout.item_post, parent, false)
+        val view = LayoutInflater.from(context).inflate(R.layout.new_post, parent, false)
         return ViewHolder(view)
     }
 
     override fun getItemCount() = posts.size
-    }
+
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         holder.bind(posts[position])
@@ -24,7 +27,10 @@ class PostsAdapter (val context: Context, val posts: List<Post>) :
 
     inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         fun bind(post: Post) {
-            itemView.itemName.text = post.user?.username
-
+            itemView.username.text = post.user?.username
+            itemView.itemName.text = post.description
+            Glide.with(context).load(post.imageUrl).into(itemView.itemImage)
+            itemView.relativeTime.text = DateUtils.getRelativeTimeSpanString(post.creationTimeMs)
         }
+    }
 }
